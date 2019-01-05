e5dd8ac1b253633bce6943cba14a414ace088cd0
L3viathan <git@l3vi.de>
2019-01-05 11:52:17 +0100

    path registry, and HEADERS contextvar

--- httriop.py
+++ httriop.py
@@ -20,26 +20,34 @@ def parse_headers(value):
 
 
 REMOTE = ContextVar("REMOTE")
+HEADERS = ContextVar("HEADERS")
+
+REGISTRY = {}
 
 
-def request(input=identity, output=identity):
+def request(path, input=identity, output=identity):
     def decorator(afn):
-        async def inner(conn):
-            bytestream = io.BytesIO()
-            r_ip, r_port, *_ = conn.socket.getpeername()
-            new = b""
-            while not new.endswith(b"\r\n"):
-                new = await conn.receive_some(1024)
-                bytestream.write(new)
-            value = bytestream.getvalue().decode("utf-8")
-            REMOTE.set((r_ip, r_port))
-            path, headers, data = parse_headers(value)
-            result = await afn(input(value))
-            await conn.send_all(output(result).encode("utf-8"))
-
-        return inner
-
+        REGISTRY[path] = (afn, input, output)
+        return afn
     return decorator
+
+
+
+async def handler(conn):
+    bytestream = io.BytesIO()
+    r_ip, r_port, *_ = conn.socket.getpeername()
+    new = b""
+    while not new.endswith(b"\r\n"):
+        new = await conn.receive_some(1024)
+        bytestream.write(new)
+    value = bytestream.getvalue().decode("utf-8")
+    path, headers, data = parse_headers(value)
+    REMOTE.set((r_ip, r_port))
+    HEADERS.set(headers)
+
+    afn, input, output = REGISTRY[path]
+    result = await afn(input(data))
+    await conn.send_all(output(result).encode("utf-8"))
 
 
 def output_json(value):
@@ -50,10 +58,10 @@ def input_json(value):
     return json.loads(value)
 
 
-async def main(handler):
+async def main():
     async with trio.open_nursery() as nursery:
         nursery.start_soon(trio.serve_tcp, handler, 8080)
 
 
-def serve(handler):
-    trio.run(main, handler)
+def serve():
+    trio.run(main)
--- server.py
+++ server.py
@@ -1,11 +1,12 @@
-from httriop import request, output_json, REMOTE, serve
+from httriop import request, output_json, REMOTE, serve, HEADERS
 
 
-@request(output=output_json)
+@request("/", output=output_json)
 async def handler(msg):
     print("received:", repr(msg), "from:", REMOTE.get())
+    print("headers:", HEADERS.get())
     return {"hello": True}
 
 
 if __name__ == '__main__':
-    serve(handler)
+    serve()
