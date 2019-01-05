c797030d56c7a1c3737a92ab47b3db7980b28bcd
L3viathan <git@l3vi.de>
2019-01-05 14:48:39 +0100

    Binding of variables in paths. More error-handling routes.

--- httriop.py
+++ httriop.py
@@ -1,5 +1,7 @@
 import io
+import builtins
 import json
+import re
 from contextvars import ContextVar
 import trio
 
@@ -13,28 +15,42 @@ def parse_headers(value):
     headers_ = headers_.split("\r\n")
     metaheader = headers_.pop(0)
     method, path, _ = metaheader.split()
-    headers_ = {
-        key: value
-        for key, value in (line.split(": ", maxsplit=1) for line in headers_)
-    }
+    try:
+        headers_ = {
+            key: value
+            for key, value in (line.split(": ", maxsplit=1) for line in headers_)
+        }
+    except Exception:
+        path = 400
+        headers_ = {}
     return method, path, headers_, rest
 
 
 remote = ContextVar("remote")
 headers = ContextVar("headers")
+variables = ContextVar("variables")
 
 REGISTRY = {}
 
 
 def request(method, path, input=identity, output=identity):
+    path = path.rstrip("/") if isinstance(path, str) else path
     def decorator(afn):
-        nonlocal input, output
+        nonlocal input, output, path
         if input == json:
             input = json.loads
         if output == json:
             output = json.dumps
 
-        REGISTRY[method, path] = (afn, input, output)
+        if isinstance(path, str):
+            vars = re.findall("<([^/:]+)(?::([^/:]+))?>", path)
+            path = re.compile(re.sub("<[^>]+>", "([^/]+)", path) + "$")
+        else:
+            vars = []
+
+        REGISTRY[
+            method, path
+        ] = (afn, vars, input, output)
         return afn
 
     return decorator
@@ -45,29 +61,59 @@ def GET(*args, **kwargs):
 
 
 def get_handler(method, path):
-    if (method, path) not in REGISTRY:
-        path = 404
+    path = path.rstrip("/") if isinstance(path, str) else path
+    bindings = {}
+    for m, p  in REGISTRY:
+        if m != method or isinstance(p, int) or not p.match(path):
+            continue
+        match = p.findall(path)
 
-    # TODO: magical handling of "/foo/<bar:int>" etc
+        afn, vars, input, output = REGISTRY[m, p]
+        for value, (name, transformation) in zip(match, vars):
+            if transformation:
+                try:
+                    value = getattr(builtins, transformation)(value)
+                except Exception:
+                    afn, _, input, output = REGISTRY[method, 400]
+                    break
+            bindings[name] = value
+        variables.set(bindings)
+        break
+    else:
+        afn, _, input, output = REGISTRY[method, 404]
+    return afn, bindings, input, output
 
-    return REGISTRY[method, path]
 
-
-async def handler(conn):
+async def get_bytes(conn):
     bytestream = io.BytesIO()
-    r_ip, r_port, *_ = conn.socket.getpeername()
     new = b""
     while not new.endswith(b"\r\n"):
         new = await conn.receive_some(1024)
         bytestream.write(new)
-    value = bytestream.getvalue().decode("utf-8")
+    return bytestream.getvalue().decode("utf-8")
+
+
+
+async def handler(conn):
+    r_ip, r_port, *_ = conn.socket.getpeername()
+    value = await get_bytes(conn)
     method, path, headers_, data = parse_headers(value)
+    afn, vars, input, output = get_handler(method, path)
+
+
     remote.set((r_ip, r_port))
     headers.set(headers_)
+    variables.set(vars)
 
-    afn, input, output = get_handler(method, path)
-    result = await afn(input(data))
-    await conn.send_all(output(result).encode("utf-8"))
+    result = ""
+    with trio.move_on_after(15):
+        try:
+            with trio.fail_after(10):
+                result = output(await afn(input(data)))
+        except trio.TooSlowError:
+            afn, vars, input, output = REGISTRY["GET", 504]
+            result = output(await afn(input(data)))
+    await conn.send_all(result.encode("utf-8"))
 
 
 async def main():
--- server.py
+++ server.py
@@ -3,15 +3,32 @@ from httriop import GET, remote, serve, headers
 
 
 @GET("/", output=json)
-async def handler(msg):
+async def root(msg):
     print("received:", repr(msg), "from:", remote.get())
     print("headers:", headers.get())
     return {"hello": True}
 
 
+@GET("/test/<foo:int>", output=json)
+async def test(msg):
+    print("received:", repr(msg), "from:", remote.get())
+    print("headers:", headers.get())
+    return {"hello": False}
+
+
 @GET(404)
-async def handler(_):
+async def notfound(_):
     return "404 Not Found!!"
+
+
+@GET(400)
+async def clienterror(_):
+    return "You dumb!"
+
+
+@GET(504)
+async def timeout(_):
+    return "too slow!"
 
 
 if __name__ == '__main__':
